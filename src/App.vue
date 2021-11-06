2baf4bcd28409c13ea18da1c856178ad5260d7d5
sdnc <dza.sara@gmail.com>
2021-11-06 23:49:00 +1030

    051121

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to the moneyyyy"/>
+    <HelloWorld msg="Ayyy Welcome to the moneyyyy"/>
   </div>
 </template>
 
