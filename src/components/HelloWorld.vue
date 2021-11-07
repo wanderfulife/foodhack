b220ae44b4a0c827eeea196bfffc014d83dfacaf
wanderfulife <johm.wander@hotmail.fr>
2021-11-07 12:41:00 +1030

    test merge2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <button type="button" onclick="alert('Hello world!')">Click Me Motherfucker!</button>
+    <button type="button" onclick="alert('Hello world!')">Click Me Motherfucker et la ca marche?!</button>
   </div>
 </template>
 
